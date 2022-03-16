9f7b2450db19cb6654bbe472e15af8d10c7ba7a9
DividedByInfinity <89877382+DividedByInfinity@users.noreply.github.com>
2022-03-16 23:29:28 +0200

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,18 +1,36 @@
-from string import digits
-from key_generator.key_generator import generate
 import secrets
+from string import ascii_letters, digits
+from xml.etree.ElementTree import tostring
 
-num = int(input('How many keys would you like to generate: '))
+key = ['n', 'a', 'a', 'n', '', 'n', 'n', 'a', 'a', '', 'a', 'a', 'n', 'n', '', 'n', 'n', 'n', 'n']
 
-for x in range(num):
-    custom_key = generate(
-        4, 
-        '-', 
-        4, 
-        4, 
-        type_of_value='char', 
-        capital='mix', 
-        extras=list(digits), 
-        seed=secrets.randbelow(9999999)
-        ).get_key()
-    print(custom_key)
+times = int(input("How many keys would you like to generate: "))
+
+for x in range(times):
+
+    alpha = []
+    letter = 0
+
+    digitz = []
+    num = 0
+
+    generated_key = ''
+
+    for letters in range(7):
+        alpha.append(secrets.choice(ascii_letters))
+
+    for numbers in range(11):
+        digitz.append(secrets.choice(digits))
+
+    for i, val in enumerate(key):
+        if key[i] == 'a':
+            generated_key += alpha[letter]
+            letter += 1
+        elif key[i] == 'n':
+            generated_key += digitz[num]
+            num += 1
+        else:
+            generated_key += '-'
+    
+    print(generated_key)
+
